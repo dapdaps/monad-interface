1d1083b6eee70127e7027740ee4f14172d5fed55
Yyy <snwyz1st@gmail.com>
2025-03-12 18:44:44 +0800

    feat: add index animate

--- src/hooks/use-rem.ts
+++ src/hooks/use-rem.ts
@@ -1,7 +1,11 @@
-import { useLayoutEffect } from 'react';
+import { useEffect } from 'react';
 import { useEventListener, useDebounceFn } from 'ahooks';
 
 function useRem(designWidth = 1440, baseFontSize = 14.4) {
+  if (typeof document === 'undefined' || !document.documentElement.style.getPropertyValue('--rem')) {
+    document.documentElement.style.setProperty('--rem', `${baseFontSize}px`);
+  }
+
   const setRem = () => {
     const scale = window.innerWidth / designWidth;
     document.documentElement.style.setProperty('--rem', `${baseFontSize * scale}px`);
@@ -11,9 +15,12 @@ function useRem(designWidth = 1440, baseFontSize = 14.4) {
     wait: 100, 
   });
 
-  useLayoutEffect(() => {
-    setRem(); 
-  }, [designWidth, baseFontSize]);
+  useEffect(() => {
+    setRem();
+    return () => {
+      document.documentElement.style.removeProperty('--rem');
+    };
+  }, []);
 
   useEventListener('resize', debounceSetRem);
 }
