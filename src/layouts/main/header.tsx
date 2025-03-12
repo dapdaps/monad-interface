2d434d90e5a75e13d2036c1a9868659cf13672c4
Yyy <snwyz1st@gmail.com>
2025-03-12 19:08:11 +0800

    fix header

--- src/layouts/main/header.tsx
+++ src/layouts/main/header.tsx
@@ -17,27 +17,34 @@ const MainLayoutHeader = (props: Props) => {
   };
   return (
     <header
-      className={`flex justify-between w-full h-[60px] stroke-black sticky font-CherryBomb top-0 z-50 ${className} bear-header`}
+      className={`flex w-full h-[60px] stroke-black sticky font-CherryBomb top-0 z-50 ${className} bear-header`}
       style={style}
     >
-      {
-        DONT_NEED_SHOW_BACK.includes(pathname) ? (
-          <div />
-        ) : (
-          <div className="cursor-pointer w-[120px]" onClick={() => {
-            router.back()
-          }}>
-            <img src="/images/header/back_button.svg" alt="back_button" />
-          </div>
-        )
-      }
+      <div className="min-w-[120px]">
+        {
+          DONT_NEED_SHOW_BACK.includes(pathname) ? (
+            <div />
+          ) : (
+            <div className="cursor-pointer w-[120px]" onClick={() => {
+              router.back()
+            }}>
+              <img src="/images/header/back_button.svg" alt="back_button" />
+            </div>
+          )
+        }
+      </div>
 
-      <div className="flex items-center justify-center w-[300px] h-[60px] bg-[url('/images/header/logo_bg.svg')]">
-        <div className="w-[169px]">
-          <img src="/images/header/logo.svg" alt="logo" />
+      <div className="flex-1 flex items-center justify-center">
+        <div className="flex items-center justify-center w-[300px] h-[60px] bg-[url('/images/header/logo_bg.svg')]">
+          <div className="w-[169px]">
+            <img src="/images/header/logo.svg" alt="logo" />
+          </div>
         </div>
       </div>
-      <ConnectWallet />
+
+      <div className="min-w-[120px] flex justify-end">
+        <ConnectWallet />
+      </div>
     </header>
   );
 };
