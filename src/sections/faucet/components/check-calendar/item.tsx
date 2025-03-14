af5f5721d2906bad4cf1eb46913bda97fd5251b5
jimmygu <jimmyguu2024@gmail.com>
2025-03-14 10:18:39 +0800

    fix: Refactor reward_amount to use Big.js and update type to string
    Utilized Big.js for safer numerical operations on reward_amount throughout the faucet module. Changed reward_amount type from number to string to ensure precision in handling rewards. Adjusted related logic and UI components accordingly.

--- src/sections/faucet/components/check-calendar/item.tsx
+++ src/sections/faucet/components/check-calendar/item.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import Big from 'big.js';
 
 const FaucetCheckCard = (props: any) => {
   const { className, item } = props;
@@ -7,7 +8,7 @@ const FaucetCheckCard = (props: any) => {
     <div
       className={clsx(
         "relative flex justify-center items-center w-[63px] h-[62px] bg-no-repeat bg-center rounded-[6px] shadow-[0_0_10px_0_rgba(0,_0,_0,_0.05)]",
-        item.checkin_date ? (item.reward_amount ? "bg-[#BFFF60]" : "bg-[#836EF9] bg-[url('/images/faucet/icon-un-check-in.svg')]") : "bg-[#3E347C] bg-[url('/images/faucet/icon-un-check-in.svg')]",
+        item.checkin_date ? (Big(item.reward_amount || 0).gt(0) ? "bg-[#BFFF60]" : "bg-[#836EF9] bg-[url('/images/faucet/icon-un-check-in.svg')]") : "bg-[#3E347C] bg-[url('/images/faucet/icon-un-check-in.svg')]",
         className
       )}
     >
@@ -15,7 +16,7 @@ const FaucetCheckCard = (props: any) => {
         !!item.checkin_date && (
           <>
             {
-              item.reward_amount ? (
+              Big(item.reward_amount || 0).gt(0) ? (
                 <>
                   <div className="text-[#3E347C] text-[12px] font-Unbounded font-[500] text-center translate-y-2.5">
                     <div>{item.reward_amount}</div>
