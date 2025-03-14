fd8ef6ee2a0b06d25821607b603702997351dee4
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2025-03-14 20:14:50 +0800

    fix: fix button bugs

--- src/sections/dapps/components/rectangular-button.tsx
+++ src/sections/dapps/components/rectangular-button.tsx
@@ -6,20 +6,23 @@ export default memo(function RectangularButton({
   clicked,
   children,
   className,
+  disabled,
   onClick
 }: {
   type: 1 | 2 | 3
   clicked: boolean
   children: ReactDOM
   clssName: string
-  onClick: any
+  disabled: boolean
+  onClick?: VoidFunction
 }) {
   return (
-    <div className={clsx("cursor-pointer relative flex  border border-black rounded-[6px] transform", type === 1 ? "skew-x-[-5deg]" : (type === 2 ? "skew-x-[5deg]" : ""), clicked ? 'bg-[#A5FFFD] drop-shadow-[0_0_10px_rgba(165, 255, 253, 0.60)]' : 'bg-[#A6A6D2] drop-shadow-[0_6px_0_#000]', className)} onClick={onClick}>
+    <div className={clsx("relative flex  border border-black rounded-[6px] transform", disabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer ", type === 1 ? "skew-x-[-5deg]" : (type === 2 ? "skew-x-[5deg]" : ""), clicked ? 'bg-[#A5FFFD] drop-shadow-[0_0_10px_rgba(165, 255, 253, 0.60)]' : 'bg-[#A6A6D2] drop-shadow-[0_6px_0_#000]', className)} onClick={onClick}>
       <div className={clsx("absolute left-0 right-0 bottom-0 top-0", type === 1 ? "skew-x-[5deg]" : (type === 2 ? "skew-x-[-5deg]" : ""))}>
         <div className={clsx("absolute w-1/2 top-0 bottom-0 rounded-[6px] border-black", type === 1 ? "-right-[6px] border-r" : (type === 2 ? "-left-[6px] border-l" : "hidden"), clicked ? "bg-[#A5FFFD]" : "bg-[#A6A6D2]")} />
         <div className="absolute left-0 top-0 right-0 bottom-0 flex justify-center items-center text-black font-Unbounded text-[12px] font-medium ">{children}</div>
       </div>
     </div>
+
   )
 })
