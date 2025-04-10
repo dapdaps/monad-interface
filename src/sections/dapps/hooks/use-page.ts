a135f168c510c9f251450808e7ecea3200633cbf
jimmygu <jimmyguu2024@gmail.com>
2025-04-10 12:29:44 +0800

    fix: feedback-2#Refactor dapps filtering and pagination logic
    Streamline the logic for filtering dapps based on `activeType` and reorganize pagination to handle `outlink` and `instation` dapps separately. This improves clarity and adjusts the layout logic to better utilize the `maxLength` parameter.

--- src/sections/dapps/hooks/use-page.ts
+++ src/sections/dapps/hooks/use-page.ts
@@ -10,26 +10,51 @@ export default function usePage() {
   const [activeType, setActiveType] = useState("all");
   const maxLength = useMemo(() => isMobile ? 2 : Math.floor(((size?.width - 64) * 0.8 + 80) / 240), [size])
   const dappsArray = useMemo(() => {
-    const outlinkDapps = ALL_DAPP_LIST.filter((dapp: IDapp) =>
-      dapp.link.indexOf('http') > -1
+    const dapps = ALL_DAPP_LIST.filter(
+      (dapp: IDapp) => {
+        if (activeType === "all" ||
+          activeType === "instation" && dapp.link.indexOf("http") === -1 ||
+          activeType === "outlink" && dapp.link.indexOf("http") > -1
+        ) {
+          return true
+        }
+        return dapp.type === activeType
+      }
     );
-    const otherDapps = ALL_DAPP_LIST.filter((dapp: IDapp) =>
-      activeType === 'all'
-        ? dapp.link.indexOf('http') === -1 || dapp.link.indexOf('http') > -1
-        : activeType === 'instation'
-          ? dapp.link.indexOf('http') === -1
-          : dapp.type === activeType
-    );
+    const outlinkDApps = dapps.filter((dapp: IDapp) => dapp.link.indexOf("http") > -1);
+    const otherDApps = dapps.filter((dapp: IDapp) => dapp.link.indexOf("http") < 0);
+    const array: any = []
 
-    const combinedDapps =
-      activeType === 'all' || activeType === 'outlink'
-        ? [...otherDapps.filter(dapp => outlinkDapps.indexOf(dapp) === -1), ...outlinkDapps]
-        : otherDapps;
+    let arrayIndex = 0;
+    for (let i = 0; i < otherDApps.length; i++) {
+      if (!array[arrayIndex]) {
+        array[arrayIndex] = [otherDApps[i]];
+      } else {
+        array[arrayIndex].push(otherDApps[i]);
+      }
 
-    const array = [];
-    for (let i = 0; i < combinedDapps.length; i += maxLength) {
-      array.push(combinedDapps.slice(i, i + maxLength));
+      if (i - maxLength * arrayIndex >= maxLength - 1) {
+        arrayIndex += 1;
+      }
     }
+    if (!array[arrayIndex] || array[arrayIndex].length < maxLength) {
+      arrayIndex += 1;
+    }
+    const _prevArrayIndex = arrayIndex;
+    for (let i = 0; i < outlinkDApps.length; i++) {
+      if (!array[arrayIndex]) {
+        array[arrayIndex] = [outlinkDApps[i]];
+      } else {
+        array[arrayIndex].push(outlinkDApps[i]);
+      }
+
+      if (i - maxLength * (arrayIndex - _prevArrayIndex) >= maxLength - 1) {
+        arrayIndex += 1;
+      }
+    }
+    console.log("array: %o", array);
+    console.log("arrayIndex: %o", arrayIndex);
+    console.log("maxLength: %o", maxLength);
     return array;
   }, [activeType, maxLength]);
 
