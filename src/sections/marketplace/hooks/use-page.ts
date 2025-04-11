f37ebb275127d13dda1a70c1cbdf11b75026c1eb
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2025-04-11 16:44:13 +0800

    fix: fix some bugs

--- src/sections/marketplace/hooks/use-page.ts
+++ src/sections/marketplace/hooks/use-page.ts
@@ -147,7 +147,7 @@ export default function usePage() {
       let next_level = false;
       if (index === col - 1 || hasNextLevel(sortTokens, i, key)) {
         index = 0
-        start_x = Math.random() * 20 + 10
+        start_x = Math.random() * 20 + 15
         start_y += h
       } else {
         index += 1
