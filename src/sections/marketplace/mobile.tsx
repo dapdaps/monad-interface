d53770c71f9dbb27f5aa8f7c42565601b8f19216
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2025-04-16 11:01:01 +0800

    fix: fix mobile marketplace tokens font

--- src/sections/marketplace/mobile.tsx
+++ src/sections/marketplace/mobile.tsx
@@ -4,6 +4,7 @@ import Jar from "./components/jar"
 import Background from "./components/mobile/background"
 import usePage from "./hooks/use-page"
 import Operation from "./components/operation"
+import TokensFont from '@public/images/marketplace/mobile/tokens_font.svg'
 
 export default memo(function Mobile() {
   const { tokensArray, activeType, setActiveType } = usePage()
@@ -11,7 +12,7 @@ export default memo(function Mobile() {
   return (
     <Background>
       <div className="flex justify-center h-full overflow-auto">
-        <Jar title="Tokens" tokens={tokens} type={activeType} />
+        <Jar title={<TokensFont />} tokens={tokens} type={activeType} />
       </div>
       <Operation activeType={activeType} setActiveType={setActiveType} />
     </Background>
