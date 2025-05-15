007b18bcd508c943a9142dfbc129eb21229eb95b
Emily <emily@ref.finance>
2025-05-15 16:56:23 +0800

    some changes for chat login.

--- src/sections/chat/login.tsx
+++ src/sections/chat/login.tsx
@@ -85,14 +85,18 @@ const LoginView = (props: any) => {
               className="mt-[33px] w-[445px] h-[22px] object-center object-contain shrink-0"
             />
             <div className="mt-[50px] w-[440px] shrink-0 flex flex-col items-center">
-              <div className="shrink-0">Enter Name</div>
-              <input
-                type="text"
-                className="w-full h-[52px] shrink-0 rounded-[2px] border border-[#8645FF] bg-black text-center mt-[22px]"
-                value={name || ""}
-                onChange={(e) => onNameChange(e.target.value)}
-                maxLength={50}
-              />
+              {address && (
+                <>
+                  <div className="shrink-0">Enter Name</div>
+                  <input
+                    type="text"
+                    className="w-full h-[52px] shrink-0 rounded-[2px] border border-[#8645FF] bg-black text-center mt-[22px]"
+                    value={name || ""}
+                    onChange={(e) => onNameChange(e.target.value)}
+                    maxLength={50}
+                  />
+                </>
+              )}
               <Button
                 className="mt-[26px] h-[52px] disabled:!cursor-not-allowed disabled:opacity-30 shrink-0 w-full rounded-[2px] text-white text-center font-SpaceMono text-[16px] font-normal leading-[90%]"
                 disabled={buttonDisabled}
