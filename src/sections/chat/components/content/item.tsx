12d395ab32906f897450df28d9c0be560bec9a7c
Emily <emily@ref.finance>
2025-05-16 15:23:23 +0800

    add error adaption.

--- src/sections/chat/components/content/item.tsx
+++ src/sections/chat/components/content/item.tsx
@@ -6,12 +6,12 @@ export default function Item({ message, user }: any) {
     <div
       className="flex gap-[8px] text-[16px] leading-[200%]"
       style={{
-        color: user.role ? "#7B23FF" : "white"
+        color: user?.role ? "#7B23FF" : "white"
       }}
     >
       <div className="shrink-0">
-        [{dayjs(message.timestamp).format("HH:mm:ss")}] [{user.name}]{" "}
-        <Level level={user.level} />:
+        [{dayjs(message.timestamp).format("HH:mm:ss")}] [{user?.name}]{" "}
+        <Level level={user?.level} />:
       </div>
       <div>{message.text}</div>
     </div>
