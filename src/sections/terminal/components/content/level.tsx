2ecef63bab683f4db2340a7e27c32eb3f568bec3
jimmygu <jimmyguu2024@gmail.com>
2025-05-19 17:26:24 +0800

    feat: Refactor level icons and panel to use LEVELS configuration
    Replaced hardcoded level information with a dynamic LEVELS config object. Updated the components to leverage the new configuration for better maintainability and consistency. Added tooltips in `LevelIcon` to display detailed level info interactively.

--- src/sections/terminal/components/content/level.tsx
+++ src/sections/terminal/components/content/level.tsx
@@ -1,8 +1,29 @@
-import icons from "./level-icon";
 import { memo } from "react";
+import Popover, { PopoverPlacement, PopoverTrigger } from '@/components/popover';
+import { LEVELS } from '@/sections/terminal/config';
 
 export default memo(function LevelIcon({ level }: any) {
   if (!level) return "";
 
-  return level.split(",").map((it: string, i: number) => `[${icons[it]}]`);
+  return level.split(",").map((it: string, i: number) => {
+    const currLevel = (LEVELS as any)[it];
+    return (
+      <Popover
+        key={i}
+        content={(
+          <div className="p-[11px_18px] flex-shrink-0 rounded-[6px] bg-[#212041] text-[#CEB4F5] font-SpaceMono text-[14px] leading-[200%]">
+            ({currLevel?.value})"{currLevel?.name}": {currLevel?.condition}
+          </div>
+        )}
+        trigger={PopoverTrigger.Hover}
+        placement={PopoverPlacement.TopLeft}
+        closeDelayDuration={0}
+        triggerContainerClassName="inline"
+      >
+      <span key={i} className="">
+      [{currLevel?.value}]
+    </span>
+      </Popover>
+    );
+  });
 });
--- src/sections/terminal/components/footer/level-panel.tsx
+++ src/sections/terminal/components/footer/level-panel.tsx
@@ -1,20 +1,20 @@
 import clsx from 'clsx';
+import { LEVELS } from '@/sections/terminal/config';
 
 export default function LevelPanel(props: any) {
   const { className } = props;
 
   return (
     <div className={clsx("w-[412px] bg-[#18162B] p-[14px_24px_16px_15px] text-[#CEB4F5] text-[14px] leading-[30px]", className)}>
-      (✦)"Peasant": Hold &lt;10 MON <br />
-      (✧)"Worker": Hold 10-50 MON <br />
-      (✬)"Middle class": Hold 50-100 MON <br />
-      (✭)"Business": Hold 100-1000 MON <br />
-      (۞)"Wealthy": Hold &gt;1000 MON <br />
-      (✻)"NGMI": Have &lt;5 txns on testnet <br />
-      (✾)"Lazy": Have 5-20 txns on testnet <br />
-      (✿)"Standard": Have 20-100 txns on testnet <br />
-      (❀)"Hardwork": Have 100-500 txns on testnet <br />
-      (❂)"Certified": Have &gt;500 txns on testnet
+      {
+        Object.values(LEVELS).map((it, idx) => {
+          return (
+            <>
+              ({it.value})"{it.name}": {it.condition} {idx < Object.values(LEVELS).length - 1 && <br />}
+            </>
+          );
+        })
+      }
     </div>
   );
 }
