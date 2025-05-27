c10ce4d14426dd0595a5ee21de98a6479555e0ac
ben <benjm240@gmail.com>
2025-05-27 17:36:50 +0800

    add nft twitter

--- src/components/nft/NadsaPassCard.tsx
+++ src/components/nft/NadsaPassCard.tsx
@@ -1,11 +1,17 @@
 import { useNFT } from "@/hooks/use-nft";
 import CircleLoading from "../circle-loading";
 import { useMemo } from "react";
+import { useAppKit } from "@reown/appkit/react";
+import { useAccount } from "wagmi";
+import { useSwitchChain } from "wagmi";
+import { monadTestnet } from "@reown/appkit/networks";
+import useBindTwitterAccount from "@/sections/terminal/hooks/use-bind-twitter-account";
 
 export default function NadsaPassCard() {
     const { nftMetadata, nftAddress, mintNFT, hasNFT, tokenIds, isLoading, address } = useNFT();
-
-    console.log(nftMetadata, hasNFT, tokenIds);
+    const { open } = useAppKit();
+    const { isSuccess: isBindTwitterAccount } = useBindTwitterAccount({ withAuth: false });
+    console.log(nftMetadata, hasNFT, tokenIds, isBindTwitterAccount);
 
     const status = useMemo(() => {
         if (!address) {
@@ -79,17 +85,26 @@ export default function NadsaPassCard() {
                                 <div className="text-[#00FF00]">
                                     <RightArrow />
                                 </div>
-                            </div> : <div className="flex items-center justify-between mt-[10px] text-[12px] h-[40px] px-[10px] bg-[#7663F4] rounded-[2px] font-Pixelmix">
+                            </div> : <button onClick={() => {
+                                open();
+                            }} className="flex items-center justify-between mt-[10px] text-[12px] h-[40px] px-[10px] bg-[#7663F4] rounded-[2px] font-Pixelmix">
                                 <div className="text-[#FFFFFF]">Connect Wallet</div>
+                            </button>
+                        }
+
+                        {
+                            !isBindTwitterAccount ? <button onClick={() => {
+                                window.open('https://twitter.com/intent/follow?screen_name=0xNADSA', '_blank');
+                            }} className="flex items-center justify-center mt-[10px] text-[12px] w-full h-[40px] px-[10px] bg-[#7663F4] rounded-[2px] font-Pixelmix">
+                                <div className="text-[#FFFFFF]">Follow 0xNADSA on X</div>
+                            </button> : <div className="flex items-center justify-between mt-[10px] text-[12px] h-[40px] px-[10px] bg-[#212041] rounded-[2px] font-Pixelmix">
+                                <div className="text-[#FFFFFF]">Followed 0xNADSA</div>
+                                <div className="text-[#00FF00]">
+                                    <RightArrow />
+                                </div>
                             </div>
                         }
 
-                        <div className="flex items-center justify-between mt-[10px] text-[12px] h-[40px] px-[10px] bg-[#212041] rounded-[2px] font-Pixelmix">
-                            <div className="text-[#FFFFFF]">Followed 0xNADSA</div>
-                            <div className="text-[#00FF00]">
-                                <RightArrow />
-                            </div>
-                        </div>
                     </>
                 }
 
@@ -102,9 +117,9 @@ export default function NadsaPassCard() {
                 }
                 {
                     status === 0 && (
-                        <button onClick={() => mintNFT()} className="flex w-full items-center justify-center text-[12px] h-[40px] mt-[10px] mb-[10px] bg-[#7663F4] text-[#fff] rounded-[2px] font-Pixelmix">
+                        <MainBtn onClick={() => mintNFT()} >
                             {isLoading ? <><CircleLoading /> Mint NFT</> : 'Mint NFT'}
-                        </button>
+                        </MainBtn>
                     )
                 }
                 {
@@ -124,5 +139,33 @@ const RightArrow = () => {
         <svg width="17" height="12" viewBox="0 0 17 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M1 4.5L6.5 10L15.5 1" stroke="#00FF11" stroke-width="2" />
         </svg>
+    )
+}
+
+const MainBtn = ({ onClick, children }: { onClick: any, children: any }) => {
+    const { switchChain, isPending: switching } = useSwitchChain();
+    const { address, chainId } = useAccount();
+    const { open } = useAppKit();
+
+    if (!address) {
+        return (
+            <button onClick={() => open()} className="flex w-full items-center justify-center text-[12px] h-[40px] mt-[10px] mb-[10px] bg-[#7663F4] text-[#fff] rounded-[2px] font-Pixelmix">
+                Connect
+            </button>
+        )
+    }
+
+    if (chainId !== monadTestnet.id) {
+        return (
+            <button onClick={() => switchChain({ chainId: monadTestnet.id })} className="flex w-full items-center justify-center text-[12px] h-[40px] mt-[10px] mb-[10px] bg-[#7663F4] text-[#fff] rounded-[2px] font-Pixelmix">
+                Switch to Monad
+            </button>
+        )
+    }
+
+    return (
+        <button onClick={onClick} className="flex w-full items-center justify-center text-[12px] h-[40px] mt-[10px] mb-[10px] bg-[#7663F4] text-[#fff] rounded-[2px] font-Pixelmix">
+            {children}
+        </button>
     )
 }
