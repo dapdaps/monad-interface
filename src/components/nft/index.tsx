23e35be26b38e0e73a0b027b57174e78b33f5e95
jimmygu <jimmyguu2024@gmail.com>
2025-06-04 19:16:55 +0800

    feat: add link of homepage to terminal

--- src/components/nft/index.tsx
+++ src/components/nft/index.tsx
@@ -1,5 +1,6 @@
 import Modal from "@/components/modal";
 import NadsaPassCard from "./NadsaPassCard";
+import Link from "next/link";
 
 export default function NFT({
   isOpen,
@@ -23,8 +24,12 @@ export default function NFT({
       isShowCloseIcon={false}
       onClose={closeModal}
       className={className}
+      innerClassName="flex flex-col items-center gap-[30px]"
     >
       <NadsaPassCard onLoginOut={onLoginOut} />
+      <Link href="/" prefetch={true} className="flex justify-center items-end w-[290px] h-[132px] bg-[url('/images/terminal/bg-landing-homepage.png')] bg-no-repeat bg-center bg-contain text-white text-[16px] font-Pixelmix font-normal leading-[200%] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
+        <div className="pb-[14px]">LANDING NADSA &gt;</div>
+      </Link>
     </Modal>
   );
 }
