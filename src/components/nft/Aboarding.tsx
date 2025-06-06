7fdec0d771b7ba00ba68227669a02f4abd759e31
ben <benjm240@gmail.com>
2025-06-06 16:33:58 +0800

    add nft store

--- src/components/nft/Aboarding.tsx
+++ src/components/nft/Aboarding.tsx
@@ -2,7 +2,6 @@ import React, { useCallback, useMemo, useRef, useState } from "react";
 import Modal from "@/components/modal";
 import { useNFT } from "@/hooks/use-nft";
 import { useAppKit } from "@reown/appkit/react";
-import { useUserStore } from "@/stores/user";
 import CircleLoading from "../circle-loading";
 import { useAccount } from "wagmi";
 import { useSwitchChain } from "wagmi";
@@ -17,6 +16,7 @@ import clsx from "clsx";
 import { sleep } from "@/sections/bridge/lib/util";
 import useTokenBalance from "@/hooks/use-token-balance";
 import { toast } from "react-toastify";
+import { useNftStore } from "@/stores/nft";
 
 const slides = [
   {
@@ -73,8 +73,8 @@ export default function Aboarding({
   const nftCardRef = useRef<HTMLDivElement>(null);
   const { loading: binding, buttonText } = useBindTwitterHome();
   const [isSharing, setIsSharing] = useState(false);
-  const closeNFTModal = useUserStore((store: any) => store.closeNFTModal);
-  const setUserInfo = useUserStore((store: any) => store.set);
+  const closeNFTModal = useNftStore((store: any) => store.closeNFTModal);
+  const setNftStore = useNftStore((store: any) => store.set);
   const index = address ? getVisitedIndex(address) : 0;
   const { tokenBalance, isLoading: isTokenBalanceLoading } = useTokenBalance(
     "native",
@@ -98,7 +98,7 @@ export default function Aboarding({
 
   const handleStart = () => {
     closeModal();
-    setUserInfo({ closeNFTModal: true });
+    setNftStore({ closeNFTModal: true });
   };
 
   const { img, title, desc, descTitle } = useMemo(
@@ -150,7 +150,7 @@ export default function Aboarding({
       open={isOpen}
       isMaskClose={false}
       onClose={() => {
-        setUserInfo({ closeNFTModal: true });
+        setNftStore({ closeNFTModal: true });
         closeModal();
       }}
       className={className}
