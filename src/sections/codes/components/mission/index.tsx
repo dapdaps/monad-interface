fbeedf4aea1126ffe525c1fbf38f563f402a78ab
jimmygu <jimmyguu2024@gmail.com>
2025-06-09 12:25:43 +0800

    feat: add mission component api

--- src/sections/codes/components/mission/index.tsx
+++ src/sections/codes/components/mission/index.tsx
@@ -1,81 +1,55 @@
 import clsx from "clsx";
 import { memo } from "react";
-
-const MISSION_CODES = new Map([
-  [
-    1,
-    {
-      codes: 1,
-      completed: true,
-      mission: "",
-    },
-  ],
-  [
-    2,
-    {
-      codes: 2,
-      completed: true,
-      mission: "",
-    },
-  ],
-  [
-    3,
-    {
-      codes: 2,
-      completed: true,
-      mission: "",
-    },
-  ],
-  [
-    4,
-    {
-      codes: 3,
-      completed: false,
-      mission: "",
-    },
-  ],
-  [
-    5,
-    {
-      codes: 3,
-      completed: false,
-      isMore: true,
-      mission: "",
-    },
-  ],
-]);
+import { useMission } from "../../hooks/use-mission";
+import Skeleton from "react-loading-skeleton";
+import Loading from "@/components/loading";
 
 export default memo(function Mission() {
+  const { missionData, missionLoading, lastTime, consecutiveList, currentRountCodes } = useMission();
 
+  const { current_round_quest = {}, current_round_complete } = missionData ?? {};
+  const { title, action_type, token, token_amount, times } = current_round_quest;
 
   return (
     <div className="flex flex-col items-center">
       <div className="text-white font-Unbounded text-[18px]">Global Mission</div>
       <div className="m-[13px_0_19px] text-[#A6A6DB] font-Unbounded text-[12px] font-light">Complete more, unlock more invites. The progress bar will be reset if you missed 3 in a row</div>
       <div className="w-[449px] h-[42px] p-[0_10px_0_16px] rounded-[6px] border border-[#26274B] bg-[#31305A] flex items-center justify-between text-white font-Unbounded text-[14px]">
-        <span>Complete 3 swaps in NADSA</span>
+        <span>{missionLoading ? (
+          <Skeleton width={150} height={21} borderRadius={4} />
+        ) : title}</span>
         <div className="flex items-center gap-[12px]">
-          <svg width="18" height="13" viewBox="0 0 18 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 5.34783L6.77778 11L17 1" stroke="#78FEFF" stroke-width="2" />
-          </svg>
-          <span>+1 Codes</span>
+          {
+            current_round_complete && (
+              <svg width="18" height="13" viewBox="0 0 18 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 5.34783L6.77778 11L17 1" stroke="#78FEFF" stroke-width="2" />
+              </svg>
+            )
+          }
+          <div className="flex items-center gap-[2px]">+{missionLoading ? (
+            <Loading size={14} />
+          ) : currentRountCodes} Codes</div>
         </div>
       </div>
 
       <div className="m-[15px_0_35px] flex items-center gap-[7px] text-[12px] font-Unbounded font-light">
-        <span className="text-[#A6A6DB]">Next Mission in</span>
-        <span className="text-white">9 h 59m 23s</span>
+        <span className="text-[#A6A6DB]">Next Mission {/^\d+/.test(lastTime) ? "in" : ""}</span>
+        <span className="text-white">{missionLoading ? (
+          <Skeleton width={85} height={18} borderRadius={4} />
+        ) : lastTime}</span>
       </div>
 
       <div className="w-full h-[10px] grid grid-cols-5 gap-0 mb-[82px]">
         {
-          Array.from(MISSION_CODES.values()).map((item, index) => (
-            <ProgressItem
-              key={index}
-              data={item}
-              progress={index < MISSION_CODES.size - 1 ? Array.from(MISSION_CODES.values())[index + 1].completed ? 100 : (item.completed ? 50 : 0) : 0}
-            />
-          ))
+          consecutiveList.map((item, index) => {
+            return (
+              <ProgressItem
+                key={index}
+                data={item}
+                progress={index < consecutiveList?.length - 1 ? consecutiveList[index + 1].completed ? 100 : (item.completed ? 50 : 0) : 0}
+              />
+            );
+          })
         }
       </div>
 
@@ -88,7 +62,7 @@ const ProgressItem = (props: any) => {
 
   const { codes, completed, isMore, mission } = data;
 
-  console.log("data: ", data);
+  console.log("progress data: ", data);
 
   return (
     <div className={clsx("h-full relative", !isMore && "border border-[#26274B]")}>
