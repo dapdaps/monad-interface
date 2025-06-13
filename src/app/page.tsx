fccac709bc7e801e8efb41409793d10c4bfd9aae
jimmygu <jimmyguu2024@gmail.com>
2025-06-13 17:24:39 +0800

    fix: remove redirect method in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 "use client";
+
 import Home from "@/sections/home";
-import { redirect } from "next/navigation";
 
 export default function Index() {
-  return redirect("/terminal");
   return <Home />;
 }
