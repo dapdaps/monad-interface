242f4df8826a70700435c23e26f62762abeed865
ben <benjm240@gmail.com>
2025-06-17 11:38:34 +0800

    add game report and nft loading style

--- src/components/nft/NadsaPassCard.tsx
+++ src/components/nft/NadsaPassCard.tsx
@@ -183,18 +183,22 @@ export default function NadsaPassCard({ onLoginOut }: any) {
                   <div className="text-[#FFFFFF]">Follow 0xNADSA on X</div>
                   {
                     !buttonText && <div
-                      className="absolute right-[-5px] top-[-5px]"
+                      className="absolute right-[10px] top-[50%] translate-y-[-50%]"
                       onClick={(e) => {
                         e.stopPropagation();
                       }}
                     >
-                      <div className="scale-[0.6]" onClick={(e) => {
+                      <div className="" onClick={(e) => {
                         e.stopPropagation();
                         checkFollowRelationship()
                       }}>
                         <div
                           className={`loader-arrow ${isCheckFollowLoading ? 'animate' : 'animate-none'}`}
-                        ></div>
+                        >
+                          <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0.200195 7.79231C0.200195 4.35144 3.12325 1.58462 6.7002 1.58462C7.69535 1.58462 8.63844 1.79947 9.48249 2.18189L9.01418 0.490781L10.9102 0L12.4169 5.43795L6.95411 6.85242L6.44628 5.02009L9.32319 4.27466C8.58308 3.77546 7.67892 3.48061 6.7002 3.48061C4.18094 3.48061 2.16205 5.42385 2.16205 7.79231C2.16228 10.1606 4.18108 12.1029 6.7002 12.1029C9.21931 12.1029 11.2381 10.1606 11.2383 7.79231H13.2002C13.2 11.233 10.277 14 6.7002 14C3.12339 14 0.200421 11.233 0.200195 7.79231Z" fill="#fff" />
+                          </svg>
+                        </div>
                       </div>
                     </div>
                   }
