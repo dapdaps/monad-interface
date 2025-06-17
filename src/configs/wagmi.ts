6d9ed9714b613acd88c69eb9d120a18aa3824910
jimmygu <jimmyguu2024@gmail.com>
2025-06-17 18:32:23 +0800

    fix: try to fix wagmi 2

--- src/configs/wagmi.ts
+++ src/configs/wagmi.ts
@@ -32,9 +32,9 @@ export const config = getDefaultConfig({
   ssr: true,
   projectId: projectId,
   chains: networks,
-  // transports: {
-  //   [monadTestnet.id]: http("https://testnet-rpc.monad.xyz"),
-  // },
+  transports: {
+    [DEFAULT_CHAIN_ID]: http("https://testnet-rpc.monad.xyz"),
+  },
 });
 
 // export const wagmiAdapter = new WagmiAdapter({
