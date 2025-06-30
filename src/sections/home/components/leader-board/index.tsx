ccd7001f208d2474089bd6c239e8a6d26e7b2641
ben <benjm240@gmail.com>
2025-06-30 15:57:47 +0800

    add home leaderboard

--- src/sections/home/components/leader-board/index.tsx
+++ src/sections/home/components/leader-board/index.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import Modal from "./modal";
 
 const LeaderBoard = () => {
+    const [dappOpen, setDappOpen] = useState(false);
+    const [nadsaOpen, setNadsaOpen] = useState(false);
     return <div className="absolute top-[38vh] right-[5vw] z-50">
-        <div className="w-[94px] h-[66px] cursor-pointer bg-[url('/images/monad/leader-board/dapp.png')] bg-no-repeat bg-contain hover:bg-[url('/images/monad/leader-board/dapp-hover.png')] transition-all duration-300">
+        <div className="w-[94px] h-[66px] cursor-pointer bg-[url('/images/monad/leader-board/dapp.png')] bg-no-repeat bg-contain hover:bg-[url('/images/monad/leader-board/dapp-hover.png')] transition-all duration-300" onClick={() => {  
+            setDappOpen(true);
+        }}>
         </div>
-        <div className="w-[94px] h-[66px] mt-[-20px] ml-[5px] cursor-pointer bg-[url('/images/monad/leader-board/nadsa.png')] bg-no-repeat bg-contain hover:bg-[url('/images/monad/leader-board/nadsa-hover.png')] transition-all duration-300">
+        <div className="w-[94px] h-[66px] mt-[-20px] ml-[5px] cursor-pointer bg-[url('/images/monad/leader-board/nadsa.png')] bg-no-repeat bg-contain hover:bg-[url('/images/monad/leader-board/nadsa-hover.png')] transition-all duration-300" onClick={() => {
+            setNadsaOpen(true);
+        }}>
         </div>
 
-        <Modal open={true} onClose={() => {}} />
+        <Modal type="dapp" open={dappOpen} onClose={() => {
+            setDappOpen(false);
+        }} />
+
+        <Modal type="nadsa" open={nadsaOpen} onClose={() => {
+            setNadsaOpen(false);
+        }} />
     </div>;
 };
 
--- src/sections/home/components/leader-board/modal.tsx
+++ src/sections/home/components/leader-board/modal.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-
+import React, { useMemo } from "react";
+import { useLeaderBoardDapp } from "./useLeaderBoard";
+import { addressFormated } from "@/utils/balance";
+import CircleLoading from "@/components/circle-loading";
 interface LeaderBoardItem {
   dapp: string;
   uniqueNads: number;
@@ -10,92 +12,129 @@ interface LeaderBoardItem {
 interface ModalProps {
   open: boolean;
   onClose: () => void;
-  data?: LeaderBoardItem[];
+  type: 'dapp' | 'nadsa';
 }
 
-const getMax = (data: LeaderBoardItem[], key: keyof LeaderBoardItem) => {
-  return Math.max(...data.map((item) => item[key] as number));
-};
+const AscIcon = () => {
+  return <svg width="10" height="14" viewBox="0 0 10 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4.18912 12.8764C4.58825 13.4295 5.41175 13.4295 5.81088 12.8764L8.1861 9.58521C8.6634 8.92384 8.19083 8 7.37522 8H2.62478C1.80917 8 1.3366 8.92384 1.8139 9.58521L4.18912 12.8764Z" fill="#836EF9" />
+    <path d="M4.18912 1.1236C4.58825 0.570536 5.41175 0.570536 5.81088 1.12359L8.1861 4.41479C8.6634 5.07616 8.19083 6 7.37522 6H2.62478C1.80917 6 1.3366 5.07616 1.8139 4.41479L4.18912 1.1236Z" fill="#3E347C" />
+  </svg>
+}
 
+const DescIcon = () => {
+  return <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3.18911 12.8764C3.58824 13.4295 4.41174 13.4295 4.81087 12.8764L7.18609 9.58521C7.66339 8.92384 7.19082 8 6.37521 8H1.62477C0.809163 8 0.336593 8.92384 0.813893 9.58521L3.18911 12.8764Z" fill="#3E347C" />
+    <path d="M3.18911 1.1236C3.58824 0.570537 4.41174 0.570537 4.81087 1.12359L7.18609 4.41479C7.66339 5.07616 7.19082 6 6.37521 6H1.62477C0.809163 6 0.336593 5.07616 0.813893 4.41479L3.18911 1.1236Z" fill="#836EF9" />
+  </svg>
+}
 
-const Modal: React.FC<ModalProps> = ({ open, onClose, data = leaderboardData }) => {
+const Sort = ({ sortType }: { sortType: 'asc' | 'desc' | null }) => {
+  if (sortType === 'asc') {
+    return <AscIcon />
+  }
+
+  if (sortType === 'desc') {
+    return <DescIcon />
+  }
+
+  return <svg width="10" height="14" viewBox="0 0 10 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4.18912 12.8764C4.58825 13.4295 5.41175 13.4295 5.81088 12.8764L8.1861 9.58521C8.6634 8.92384 8.19083 8 7.37522 8H2.62478C1.80917 8 1.3366 8.92384 1.8139 9.58521L4.18912 12.8764Z" fill="#3E347C" />
+    <path d="M4.18912 1.1236C4.58825 0.570536 5.41175 0.570536 5.81088 1.12359L8.1861 4.41479C8.6634 5.07616 8.19083 6 7.37522 6H2.62478C1.80917 6 1.3366 5.07616 1.8139 4.41479L4.18912 1.1236Z" fill="#3E347C" />
+  </svg>
+
+}
+
+const names = {
+  dapp: 'dapp',
+  nadsa: 'nad',
+}
+
+const nameKeys = {
+  dapp: 'users',
+  nadsa: 'address',
+}
+
+const Modal: React.FC<ModalProps> = ({ open, onClose, type }) => {
   if (!open) return null;
 
-  const maxUniqueNads = getMax(data, "uniqueNads");
-  const maxTransactions = getMax(data, "transactions");
-  const maxVolume = getMax(data, "volume");
+  const { leaderboard, maxUniqueNads, maxTransactions, sortType, item, sort, userFront3, transactionsFront3, loading } = useLeaderBoardDapp({ type, defaultItem: type === 'dapp' ? 'user' : 'transactions' });
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center font-Unbounded text-[12px] text-white justify-center">
-      <div className="bg-[#2B294A] rounded-xl border border-[#2B294A] shadow-lg w-full max-w-2xl relative">
-        <div className="overflow-x-auto">
-          <table className="min-w-full text-left text-white">
-            <thead>
-              <tr className="border-b border-[#38365E]">
-                <th className="py-2 px-3">dapp</th>
-                <th className="py-2 px-3">Unique nads</th>
-                <th className="py-2 px-3">Transactions</th>
-                <th className="py-2 px-3">Volume (MON)</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((item, idx) => (
-                <tr key={item.dapp} className="border-b border-[#38365E] last:border-0">
-                  <td className="py-2 px-3 whitespace-nowrap">{item.dapp}</td>
-                  <td className="py-2 px-3">
-                    <div className="flex items-center gap-2 relative">
-                      <span className="min-w-[48px] relative z-10 pl-[5px] pt-[2px]">{item.uniqueNads.toLocaleString()}</span>
-                      <div className="flex-1 absolute top-0 left-0 w-full">
-                        <div
-                          className="h-5 bg-[#836EF9]"
-                          style={{ width: `${(item.uniqueNads / maxUniqueNads) * 100}%` }}
-                        />
+    <div className="fixed inset-0 z-50 flex items-center font-Unbounded text-[12px] text-[#D7D7F6] justify-end pr-[200px]" onClick={() => {
+      onClose();
+    }}>
+      <div className="bg-[#2B294A] rounded-xl border border-[#2B294A] shadow-lg w-full max-w-2xl relative" onClick={(e) => e.stopPropagation()}>
+        <div className="overflow-x-auto min-h-[400px]">
+          {
+            loading && <div className="flex justify-center items-center min-h-[400px]">
+              <CircleLoading size={20} />
+            </div>
+          }
+
+          {
+            !loading && <table className="min-w-full text-left text-[#D7D7F6]">
+              <thead>
+                <tr className="border-b border-[#38365E]">
+                  <th className="py-2 px-3">{names[type]}</th>
+                  {
+                    type === 'dapp' && <th className="py-2 px-3 cursor-pointer select-none" onClick={() => sort('user')}>
+                      <div className="flex gap-2 items-center">
+                        <div>Unique nads</div>
+                        <Sort sortType={item === 'user' ? sortType : null} />
                       </div>
+                    </th>
+                  }
+
+                  <th className="py-2 px-3 cursor-pointer select-none" onClick={() => sort('transactions')}>
+                    <div className="flex gap-2 items-center">
+                      <div>Transactions</div>
+                      <Sort sortType={item === 'transactions' ? sortType : null} />
                     </div>
-                  </td>
-                  <td className="py-2 px-3">
-                    <div className="flex items-center gap-2 relative">
-                      <span className="min-w-[48px] relative z-10 pl-[5px] pt-[2px]">{item.transactions.toLocaleString()}</span>
-                      <div className="flex-1 absolute top-0 left-0 w-full">
-                        <div
-                          className="h-5 bg-[#836EF9]"
-                          style={{ width: `${(item.transactions / maxTransactions) * 100}%` }}
-                        />
-                      </div>
-                    </div>
-                  </td>
-                  <td className="py-2 px-3">
-                    <div className="flex items-center gap-2 relative">
-                      <span className="min-w-[48px] relative z-10 pl-[5px] pt-[2px]">{item.volume.toLocaleString()}</span>
-                      <div className="flex-1 absolute top-0 left-0 w-full">
-                        <div
-                          className="h-5 bg-[#836EF9]"
-                          style={{ width: `${(item.volume / maxVolume) * 100}%` }}
-                        />
-                      </div>
-                    </div>
-                  </td>
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {leaderboard.map((item, idx) => (
+                  <tr key={item.id} className="border-b border-[#38365E] last:border-0">
+                    <td className="py-2 px-3 whitespace-nowrap">
+                      {
+                        type === 'dapp' ? item.name : addressFormated(item.address)
+                      }
+                    </td>
+                    {
+                      type === 'dapp' && <td className="py-2 px-3">
+                        <div className="flex items-center gap-2 relative">
+                          <span className="min-w-[48px] relative z-10 pl-[5px] pt-[2px]">{item.users}</span>
+                          <div className="flex-1 absolute top-0 left-0 w-full">
+                            <div
+                              className={`h-5 ${item.users >= userFront3 ? 'bg-[#836EF9]' : 'bg-[#474590]'}`}
+                              style={{ width: `${(item.users / maxUniqueNads) * 100}%` }}
+                            />
+                          </div>
+                        </div>
+                      </td>
+                    }
+                    <td className="py-2 px-3">
+                      <div className="flex items-center gap-2 relative">
+                        <span className="min-w-[48px] relative z-10 pl-[5px] pt-[2px]">{item.transactions.toLocaleString()}</span>
+                        <div className="flex-1 absolute top-0 left-0 w-full">
+                          <div
+                            className={`h-5 ${item.transactions >= transactionsFront3 ? 'bg-[#836EF9]' : 'bg-[#474590]'}`}
+                            style={{ width: `${(item.transactions / maxTransactions) * 100}%` }}
+                          />
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          }
         </div>
       </div>
     </div>
   );
 };
-
-export const leaderboardData: LeaderBoardItem[] = [
-  { dapp: "Uniswap", uniqueNads: 5823, transactions: 39688, volume: 5336 },
-  { dapp: "Pancake", uniqueNads: 4823, transactions: 25449, volume: 6174 },
-  { dapp: "LFG", uniqueNads: 3602, transactions: 24375, volume: 42035 },
-  { dapp: "Kuru", uniqueNads: 2521, transactions: 39524, volume: 6409 },
-  { dapp: "Timeswap", uniqueNads: 1923, transactions: 51568, volume: 63582 },
-  { dapp: "Magic Eden", uniqueNads: 1602, transactions: 25707, volume: 7764 },
-  { dapp: "WooFi", uniqueNads: 1289, transactions: 1289, volume: 48900 },
-  { dapp: "LootGo", uniqueNads: 1002, transactions: 25510, volume: 2257 },
-  { dapp: "Kizzy", uniqueNads: 845, transactions: 10680, volume: 126 },
-  { dapp: "Opensea", uniqueNads: 623, transactions: 4235, volume: 4523 },
-];
 
 export default Modal;
