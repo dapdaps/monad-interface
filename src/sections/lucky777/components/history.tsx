83b085ff669890b9ed115f3b5b9dbffdc3c05d77
ben <benjm240@gmail.com>
2025-07-10 15:41:42 +0800

    fix wrong word

--- src/sections/lucky777/components/history.tsx
+++ src/sections/lucky777/components/history.tsx
@@ -192,7 +192,7 @@ function List({ type, winningOnly }: { type: string, winningOnly: boolean }) {
                                     {
                                         item.code === '666' && (<>
                                             <div className="flex-1 flex items-center gap-2 whitespace-nowrap">
-                                                WL
+                                                1 WL
                                                 <img src="/images/lucky777/chogstarrr-icon.png" alt="ML" className="w-[20px] h-[20px]" />
                                             </div>
                                             <div className="flex-1 flex items-center gap-2 justify-end text-[#78FEFF]">Pending</div>
