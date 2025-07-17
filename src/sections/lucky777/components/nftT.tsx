94bd94f07adfd50aa631ca1dbab395e79afd3d30
ben <benjm240@gmail.com>
2025-07-17 15:53:54 +0800

    fix 777 nft bg

--- src/sections/lucky777/components/nftT.tsx
+++ src/sections/lucky777/components/nftT.tsx
@@ -5,9 +5,9 @@ import { useEffect, useState } from "react";
 export default function NftT(props: any) {
   const { monadverse, monadoon, slmnd } = props;
   const nfts = [
-    { key: "monadverse", data: monadverse, img: "/images/lucky777/nft-monadverse-t.png" },
-    { key: "monadoon", data: monadoon, img: "/images/lucky777/nft-monadoon-t.png" },
-    { key: "slmnd", data: slmnd, img: "/images/lucky777/nft-slmnd-t.png" },
+    { key: "monadverse", data: monadverse, img: "/images/lucky777/nft-monadverse-t.png", cls: 'rotate-[-2deg]' },
+    { key: "monadoon", data: monadoon, img: "/images/lucky777/nft-monadoon-t.png", cls: 'rotate-[2deg]' },
+    { key: "slmnd", data: slmnd, img: "/images/lucky777/nft-slmnd-t.png", cls: 'rotate-[0deg]' },
   ].filter(nft => Number(nft.data?.remaining) > 0);
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -35,12 +35,12 @@ export default function NftT(props: any) {
             {
               nfts.length > 1 && (
                 nfts.map((nft, index) => (
-                  <img src={nft.img} alt="" className={clsx('w-[127px] absolute top-0 left-0 h-[175px]')} />
+                  <img src={nft.img} alt="" className={clsx('w-[127px] absolute top-0 left-0 h-[175px]', nft.cls)} />
                 ))
               )
             }
             {/* <img src='/images/lucky777/t-bg.png' alt="" className={clsx('w-[137px] absolute top-0 left-0 h-[180px]')} /> */}
-            <img src={nfts[currentIndex].img} alt="" className={clsx('w-[127px] absolute top-0 left-0 h-[175px]')} />
+            <img src={nfts[currentIndex].img} alt="" className={clsx('w-[127px] absolute top-0 left-0 h-[175px]', nfts[currentIndex].cls)} />
           </div>
         ))
       }
@@ -54,7 +54,7 @@ export default function NftT(props: any) {
             transition={{ duration: 0.2 }}
             className='absolute w-full h-full left-0 top-0'
           >
-            <img src={nfts[currentIndex].img} alt="" className={clsx('w-[127px] absolute top-0 left-0', nfts[currentIndex].key === 'monadoon' ? 'h-[175px]' : 'h-[175px]')} />
+            <img src={nfts[currentIndex].img} alt="" className={clsx('w-[127px] absolute top-0 left-0', nfts[currentIndex].cls, nfts[currentIndex].key === 'monadoon' ? 'h-[175px]' : 'h-[175px]')} />
             <div className="absolute right-[30px] bottom-[75px] font-Montserrat text-[14px] font-bold italic text-white rotate-[-5deg] drop-shadow-[2px_2px_0_#000] [text-shadow:0_0_2px_#000,1px_1px_0_#000,-1px_-1px_0_#000,1px_-1px_0_#000,-1px_1px_0_#000]">
               {nfts[currentIndex].data.remaining}/{nfts[currentIndex].data.total}
             </div>
