aa5f3691bc23e34524e541b81da0a132fd39b39d
jimmygu <jimmygujh@gmail.com>
2025-08-01 14:20:42 +0800

    feat: enable magma

--- src/sections/dapps/config.ts
+++ src/sections/dapps/config.ts
@@ -64,14 +64,14 @@ export const ALL_DAPP_LIST: IDapp[] = [
     desc: "aPriori is an MEV infrastructure and liquid staking protocol, designed for the parallel execution era and natively built on Monad.",
     disabled: false
   },
-  // {
-  //   name: "Magma",
-  //   icon: "/images/stake/magma/logo.png",
-  //   type: "Stake",
-  //   link: "/stake/magma",
-  //   desc: "Magma is a Liquid Staking Protocol building the first Distributed Validator on Monad and developing MEV to reduce latency by up to 4x.",
-  //   disabled: false
-  // },
+  {
+    name: "Magma",
+    icon: "/images/stake/magma/logo.png",
+    type: "Stake",
+    link: "/stake/magma",
+    desc: "Magma is a Liquid Staking Protocol building the first Distributed Validator on Monad and developing MEV to reduce latency by up to 4x.",
+    disabled: false
+  },
    {
     name: "Timeswap",
     icon: "/images/dapps/icons/timeswap.svg",
