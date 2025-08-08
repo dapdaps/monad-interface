cb23e22ac783faded7102c4a0e8fa9a8dbfaf8a7
ben <benjm240@gmail.com>
2025-08-08 13:04:58 +0800

    fix 777 bugs

--- src/sections/lucky777/components/xp.tsx
+++ src/sections/lucky777/components/xp.tsx
@@ -12,12 +12,13 @@ export default function Xp({ data, xpBalance, xpLevel }: { data: any, xpBalance:
 
     return (
         <>
-            <div className="absolute top-[135px] left-[50%] -translate-x-1/2 z-[2] w-[335px] h-[24px] cursor-pointer" onClick={() => setOpen(true)}>
+            <div className="absolute top-[135px] left-[50%] -translate-x-1/2 z-[2] w-[335px] h-[24px]" >
                 <div className="absolute left-0 top-1/2 -translate-y-1/2 flex items-center z-10">
                     <img
                         src={`${IMG_PATH}level-${level}.png`}
                         alt="xp-star"
                         className="w-[75px] h-[55px] cursor-pointer ml-[-70%]"
+                        onClick={() => setOpen(true)}
                     />
                 </div>
                 <div className="w-full relative h-full items-center font-HackerNoonV2">
@@ -27,7 +28,7 @@ export default function Xp({ data, xpBalance, xpLevel }: { data: any, xpBalance:
 
                     <div
                         className="h-[24px] bg-[#C6FF60] rounded-[12px] absolute left-0 top-1/2 -translate-y-1/2 border-2 border-[#2F3163] [box-shadow:0px_-4px_0px_0px_#00000080_inset]"
-                        style={{ width: `${(xpValue / maxXp) * 100}%`, minWidth: '40px', transition: 'width 0.3s' }}
+                        style={{ width: `${(Math.min(xpValue / maxXp, 1)) * 100}%`, minWidth: '40px', transition: 'width 0.3s' }}
                     />
 
                     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full flex justify-center  text-[18px] items-center z-10 text-black">
