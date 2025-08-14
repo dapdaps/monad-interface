8b10fc029551b9a8b6d26f890b2176b6c437ee66
jimmygu <jimmygujh@gmail.com>
2025-08-14 16:59:08 +0800

    feat: privy game wallet support mobile

--- src/components/privy-wallet/ActionModal.tsx
+++ src/components/privy-wallet/ActionModal.tsx
@@ -39,7 +39,7 @@ const ActionModal = ({
             setActiveTab("deposit");
         }
     }, [showDeposit]);
-    
+
     if (!playerAddress) {
         return null
     }
@@ -48,9 +48,12 @@ const ActionModal = ({
         <Modal open={open} closeIcon={<svg className="ml-[-20px] mt-[10px]" width="10" height="9" viewBox="0 0 10 9" fill="none">
             <path d="M5 3.375L8 0H10L6 4.5L10 9H8L5 5.625L2 9H0L4 4.5L0 0H2L5 3.375Z" fill="#A6A6DB" />
         </svg>} onClose={onClose} innerClassName="font-Montserrat">
-            <div className="relative w-[495px]">
-                <img src="/images/lucky777/modal-bg.png" className="absolute z-1 top-0 left-0 w-full h-full" />
-                <div className="relative p-6 w-[495px] max-w-full py-[50px] px-[50px]">
+            <div className="relative w-[495px] md:w-full">
+                <img
+                    src="/images/lucky777/modal-bg.png"
+                    className="absolute z-1 top-0 left-0 w-full h-full md:min-w-[calc(100%_+_10px)] md:min-h-[calc(100%_+_30px)] md:left-[-5px] md:bottom-[-15px]"
+                />
+                <div className="relative p-6 w-[495px] max-w-full py-[50px] px-[50px] md:py-[30px] md:px-[15px]">
                     {
                         !isJustDesposit && <div className="flex gap-2 justify-center">
                             <button
@@ -211,7 +214,7 @@ const Deposit = ({
                     console.log('hash', hash);
                     onClose();
 
-                    success({ 
+                    success({
                         title: 'Recharge success',
                         tx: hash,
                         chainId: monadTestnet.id,
@@ -348,23 +351,23 @@ const Withdraw = ({
                             },
                             address
                         });
-    
+
                         console.log('hash', hash);
-    
+
                         onClose();
-    
-                        success({ 
+
+                        success({
                             title: 'Withdraw success',
                             tx: hash,
                             chainId: monadTestnet.id,
-                         }, 'bottom-right');
-                    } catch(e) {
+                        }, 'bottom-right');
+                    } catch (e) {
                         setIsPending(false);
                         fail({
                             title: 'Withdraw fail',
                         }, 'bottom-right')
                     }
-                    
+
                 }}
             >
                 {isPending && <CircleLoading className="w-[20px] h-[20px] mr-5" />}
---
+++ src/components/privy-wallet/game-balance.tsx
@@ -0,0 +1,50 @@
+import clsx from "clsx";
+
+const GameBalance = (props: any) => {
+  const { className, tokenBalance, setOpen, setShowDeposit, setIsJustDesposit, showDeposit } = props;
+
+  return (
+    <div className={clsx(
+      "flex items-center justify-between bg-[#000] rounded-[10px] border border-[#8184CC] px-[clamp(1px,_0.833vw,_calc(var(--nadsa-laptop-width-base)*0.00833))] py-[clamp(1px,_0.556vw,_calc(var(--nadsa-laptop-width-base)*0.00556))] mb-[clamp(1px,_1.389vw,_calc(var(--nadsa-laptop-width-base)*0.01389))] w-full mt-[clamp(1px,_1.389vw,_calc(var(--nadsa-laptop-width-base)*0.01389))] ",
+      className,
+    )}>
+      <div className="flex items-center">
+        <span className="mr-[clamp(1px,_0.556vw,_calc(var(--nadsa-laptop-width-base)*0.00556))] flex items-center">
+          <svg
+            className="w-[clamp(1px,_1.667vw,_calc(var(--nadsa-laptop-width-base)*0.01667))] h-[clamp(1px,_1.667vw,_calc(var(--nadsa-laptop-width-base)*0.01667))] shrink-0"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="12" cy="12" r="12" fill="#836EF9" />
+            <path d="M11.959 3.65234C9.5602 3.65234 3.65234 9.58932 3.65234 12.0001C3.65234 14.4109 9.5602 20.348 11.959 20.348C14.3578 20.348 20.2657 14.4108 20.2657 12.0001C20.2657 9.58943 14.3578 3.65234 11.959 3.65234ZM10.6645 16.7737C9.65297 16.4967 6.93338 11.7158 7.20907 10.6993C7.48475 9.68266 12.2419 6.94963 13.2534 7.22668C14.265 7.50369 16.9847 12.2845 16.709 13.3011C16.4333 14.3176 11.6761 17.0507 10.6645 16.7737Z" fill="white" />
+          </svg>
+        </span>
+        <span className="text-white text-[clamp(1px,_1.111vw,_calc(var(--nadsa-laptop-width-base)*0.01111))]">
+          {Number(tokenBalance || 0).toFixed(4)}
+        </span>
+      </div>
+      <div className="cursor-pointer float-right" onClick={() => {
+        setOpen(true)
+        setShowDeposit(showDeposit + 1)
+        setIsJustDesposit(false)
+      }}>
+        <svg
+          className="w-[clamp(1px,_1.389vw,_calc(var(--nadsa-laptop-width-base)*0.01389))] h-[clamp(1px,_1.389vw,_calc(var(--nadsa-laptop-width-base)*0.01389))] shrink-0"
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect width="20" height="20" rx="4" fill="#836EF9" />
+          <path d="M13.4912 10.8004H10.7998V13.467C10.7998 13.7616 10.5615 14 10.2669 14H9.73399C9.43941 14 9.20018 13.7616 9.20018 13.467V10.8004H6.50881C6.22763 10.8004 6 10.5727 6 10.2915V9.70851C6 9.4273 6.22763 9.19964 6.50881 9.19964H9.20018V6.53298C9.20018 6.23837 9.43941 6 9.73399 6H10.2669C10.5615 6 10.7998 6.23926 10.7998 6.53298V9.19964H13.4912C13.7724 9.19964 14 9.4273 14 9.70851V10.2915C14 10.5718 13.7724 10.8004 13.4912 10.8004Z" fill="white" />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default GameBalance;
