4eaade9baac8cc8f3bc6e5dfb281344240639b0b
ben <benjm240@gmail.com>
2025-08-15 16:23:55 +0800

    fix 777 mobile loading bug

--- src/sections/lucky777/components/tiger.tsx
+++ src/sections/lucky777/components/tiger.tsx
@@ -958,6 +958,7 @@ export default memo(function Tiger(props: any) {
                 <motion.svg
                   width="22"
                   height="22"
+                  key={spinUserDataLoading ? 'spinning' : 'static'}
                   viewBox="0 0 22 22"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
