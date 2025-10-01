53e558ed27efdf482bb3dd7b1ebb833354e10eae
jimmygu <jimmygujh@gmail.com>
2025-10-01 11:39:36 +0800

    feat: guess who join modal

--- src/sections/arcade/guess-who/components/join/modal.tsx
+++ src/sections/arcade/guess-who/components/join/modal.tsx
@@ -10,6 +10,7 @@ const JoinRoomModal = (props: any) => {
       onClose={onClose}
       className="backdrop-blur-[5px]"
       isMaskClose={false}
+      closeIconClassName="translate-y-[10px]"
     >
       <div className="w-[700px] h-[656px] shrink-0 bg-[url('/images/mainnet/arcade/guess-who/bg-modal.png')] bg-no-repeat bg-center bg-contain">
         <Index {...props} />
