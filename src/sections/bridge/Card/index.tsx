eb390885f8615cc41e04baabd8c65aa59a5c5c13
ben <benjm240@gmail.com>
2025-10-03 19:30:15 +0800

    fix swap style

--- src/sections/bridge/Card/index.tsx
+++ src/sections/bridge/Card/index.tsx
@@ -1,22 +1,22 @@
 export default function Card({
-    children,
-    className,
-    style
-  }: {
-    children: React.ReactNode | null;
-    className?: string;
-    style?: React.CSSProperties;
-  }) {
-    return (
-      <div
-        style={{
-          ...style,
-          backgroundSize: '100% 100%',
-        }}
-        className={`px-[37px] py-[45px] bg-[url('/images/mainnet/bridge/dapp-bg.png?r=1')] bg-[100%_100%] bg-no-repeat md:px-[10px] md:py-[16px] ${className}`}
-      >
-        {children}
-      </div>
-    );
-  }
-  
+  children,
+  className,
+  style
+}: {
+  children: React.ReactNode | null;
+  className?: string;
+  style?: React.CSSProperties;
+}) {
+  return (
+    <div
+      style={{
+        ...style,
+        backgroundSize: '100% 100%',
+      }}
+      className={`px-[37px] py-[45px] bg-[url('/images/mainnet/bridge/dapp-bg.png?r=2')] bg-[100%_100%] bg-no-repeat md:px-[10px] md:py-[16px] ${className}`}
+    >
+      <div className="absolute z-[1] inset-0 aspect-auto left-[3%] right-[3%] top-[10%] bottom-[5%] pointer-events-none bg-black/20 backdrop-blur-[30px]"></div>
+      <div className="relative z-[2]">{children}</div>
+    </div>
+  );
+}
--- src/sections/swap/Bg/index.tsx
+++ src/sections/swap/Bg/index.tsx
@@ -1,5 +1,11 @@
 export default function Bg() {
   return (
-    <img src="/images/mainnet/bridge/dapp-bg.png?r=1" className="absolute z-[1] inset-0 aspect-auto w-full h-full pointer-events-none"/>
+    <div className="absolute z-[1] inset-0 aspect-auto w-full h-full pointer-events-none">
+      <img
+        src="/images/mainnet/bridge/dapp-bg.png?r=2"
+        className="absolute z-[1] inset-0 aspect-auto w-full h-full pointer-events-none"
+      />
+      <div className="absolute z-[1] inset-0 aspect-auto left-[3%] right-[3%] top-[10%] bottom-[5%] pointer-events-none bg-black/20 backdrop-blur-[30px]"></div>
+    </div>
   );
 }
