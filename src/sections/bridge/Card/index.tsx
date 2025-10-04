40a8812d2bb20e4f9db019f0a87fbb8378b0bc48
ben <benjm240@gmail.com>
2025-10-04 10:56:29 +0800

    change style

--- src/sections/bridge/Card/index.tsx
+++ src/sections/bridge/Card/index.tsx
@@ -13,9 +13,13 @@ export default function Card({
         ...style,
         backgroundSize: '100% 100%',
       }}
-      className={`px-[37px] py-[45px] bg-[url('/images/mainnet/bridge/dapp-bg.png?r=2')] bg-[100%_100%] bg-no-repeat md:px-[10px] md:py-[16px] ${className}`}
+      className={`px-[37px] py-[45px] bg-[url('/images/mainnet/bridge/dapp-bg.png?r=3')] bg-[100%_100%] bg-no-repeat md:px-[10px] md:py-[16px] ${className}`}
     >
-      <div className="absolute z-[1] inset-0 aspect-auto left-[3%] right-[3%] top-[10%] bottom-[5%] pointer-events-none bg-black/20 backdrop-blur-[30px]"></div>
+      <div className="absolute z-[1] inset-0 aspect-auto left-[3%] right-[3%] top-[10%] bottom-[1px] pointer-events-none bg-black/10 backdrop-blur-[30px]"
+        style={{
+          clipPath: "polygon(32px 0, calc(100% - 32px) 0, 100% 32px, 100% calc(100% - 32px), calc(100% - 32px) 100%, 32px 100%, 0 calc(100% - 32px), 0 32px)"
+        }}
+      ></div>
       <div className="relative z-[2]">{children}</div>
     </div>
   );
--- src/sections/swap/Bg/index.tsx
+++ src/sections/swap/Bg/index.tsx
@@ -5,7 +5,11 @@ export default function Bg() {
         src="/images/mainnet/bridge/dapp-bg.png?r=2"
         className="absolute z-[1] inset-0 aspect-auto w-full h-full pointer-events-none"
       />
-      <div className="absolute z-[1] inset-0 aspect-auto left-[3%] right-[3%] top-[10%] bottom-[5%] pointer-events-none bg-black/20 backdrop-blur-[30px]"></div>
+      <div className="absolute z-[1] inset-0 aspect-auto left-[3%] right-[3%] top-[10%] bottom-[1px] pointer-events-none bg-black/20 backdrop-blur-[30px]"
+        style={{
+          clipPath: "polygon(32px 0, calc(100% - 32px) 0, 100% 32px, 100% calc(100% - 32px), calc(100% - 32px) 100%, 32px 100%, 0 calc(100% - 32px), 0 32px)"
+        }}
+      ></div>
     </div>
   );
 }
