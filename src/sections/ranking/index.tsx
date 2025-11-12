00255e8ee13a13a3a715556c171bda8c229b4b63
ben <benjm240@gmail.com>
2025-11-12 11:52:17 +0800

    add layout

--- src/sections/ranking/index.tsx
+++ src/sections/ranking/index.tsx
@@ -7,6 +7,10 @@ export default function Ranking() {
             <Bg />
             <div className="relative z-[1] pt-[30px]">
                 <Title />
+                <div className="flex gap-[5%] mt-[35px] mx-auto max-w-[1200px]">
+                    <div className="w-[33%] bg-[#1B1B22] text-white">left</div>
+                    <div className="flex-1 bg-[#1B1B22] text-white">right</div>
+                </div>
             </div>
         </div>
     )
