4f19a6273775ed97d3b99eb0e632de2a1cfdea6a
ben <benjm240@gmail.com>
2025-11-12 11:41:25 +0800

    add title

---
+++ src/sections/ranking/components/title/index.tsx
@@ -0,0 +1,7 @@
+export default function Title() {
+    return (
+        <div className="flex items-center justify-center relative z-[1]">
+            <img src="/images/wallet/ranking/title.png" alt="title" className="w-[229px]" />
+        </div>
+    )
+}
--- src/sections/ranking/index.tsx
+++ src/sections/ranking/index.tsx
@@ -1,11 +1,12 @@
 import Bg from "./components/bg";
+import Title from "./components/title";
 
 export default function Ranking() {
     return (
         <div className="w-full h-full bg-black">
             <Bg />
-            <div className="relative z-[1]">
-                
+            <div className="relative z-[1] pt-[30px]">
+                <Title />
             </div>
         </div>
     )
