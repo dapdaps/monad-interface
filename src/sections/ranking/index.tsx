ac338efbfdd6e2729633651b971cbb57e70541cc
ben <benjm240@gmail.com>
2025-11-12 11:18:22 +0800

    add some styles

--- src/sections/ranking/index.tsx
+++ src/sections/ranking/index.tsx
@@ -1,6 +1,12 @@
+import Bg from "./components/bg";
+
 export default function Ranking() {
     return (
         <div className="w-full h-full bg-black">
+            <Bg />
+            <div className="relative z-[1]">
+                
+            </div>
         </div>
     )
 }
