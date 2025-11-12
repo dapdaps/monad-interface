d1248dfa3133ef40e1b1a1c433e5eae3e77ecc92
jimmygu <jimmygujh@gmail.com>
2025-11-12 12:16:11 +0800

    feat: integrate RankingTabsView component into ranking layout

--- src/sections/ranking/index.tsx
+++ src/sections/ranking/index.tsx
@@ -1,4 +1,5 @@
 import Bg from "./components/bg";
+import RankingTabsView from "./components/tabs";
 import Title from "./components/title";
 
 export default function Ranking() {
@@ -9,7 +10,9 @@ export default function Ranking() {
                 <Title />
                 <div className="flex gap-[5%] mt-[35px] mx-auto max-w-[1200px]">
                     <div className="w-[33%] bg-[#1B1B22] text-white">left</div>
-                    <div className="flex-1 bg-[#1B1B22] text-white">right</div>
+                    <div className="flex-1 bg-[#1B1B22] text-white">
+                        <RankingTabsView />
+                    </div>
                 </div>
             </div>
         </div>
