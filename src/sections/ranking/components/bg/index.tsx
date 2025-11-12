d783758f718bcc2efb341073d0e7ad77dc06bd27
ben <benjm240@gmail.com>
2025-11-12 11:46:35 +0800

    add title

--- src/sections/ranking/components/bg/index.tsx
+++ src/sections/ranking/components/bg/index.tsx
@@ -1,9 +1,7 @@
 export default function Bg() {
     return (
         <div
-            className="absolute left-[-160px] opacity-30 top-1/2 -translate-y-1/2 w-[788px] h-[788px] rounded-full [background:radial-gradient(50%_50%_at_50%_50%,_#836EF9_0%,_rgba(77,65,147,0)_100%)]"
-        >
-            
-        </div>
+            className="absolute left-[-160px] opacity-30 top-1/2 -translate-y-1/2 w-[788px] h-[788px] [background:radial-gradient(50%_50%_at_50%_50%,_#836EF9_0%,_rgba(77,65,147,0)_100%)]"
+        ></div>
     )
 }
