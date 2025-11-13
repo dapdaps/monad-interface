d39b7483f824d8e6b31d901993f25334b6611c41
ben <benjm240@gmail.com>
2025-11-13 15:26:00 +0800

    add log

--- src/sections/ranking/components/userInfo/index.tsx
+++ src/sections/ranking/components/userInfo/index.tsx
@@ -66,6 +66,8 @@ export default function UserInfo() {
         return window.location.origin.includes('localhost') ? window.location.origin : 'https://alpha.nadsa.space/api/twitter_auth';
     }, []);
 
+    console.log('callbackUrl', callbackUrl);
+
     return (
         <div
             className="w-full relative"
