0f50f59526b8271b21a74f8d8dbfc4b46acca3d0
jimmygu <jimmygujh@gmail.com>
2025-11-17 16:20:09 +0800

    Refactor UserInfo component to remove unused address formatting logic and directly display formatted user name or address. Clean up imports and console logs for improved code clarity.

--- src/sections/ranking/components/userInfo/index.tsx
+++ src/sections/ranking/components/userInfo/index.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { useEffect, useMemo } from "react";
+
+import { useMemo } from "react";
 import { useUser } from "@/hooks/use-user";
 import { formatLongText } from "@/utils/utils";
 import Copyed from "@/components/copyed";
 import { EMilitaryRank, MilitaryRank } from "../../config";
-import { useAccount } from "wagmi";
 import Tip from "../tip";
 import { useUserRanking } from "../../hooks/use-user-ranking";
 import { shareReferral } from "../../lib";
@@ -12,12 +12,6 @@ import { shareReferral } from "../../lib";
 export default function UserInfo() {
     const { userInfo } = useUser();
     const { userRanking } = useUserRanking();
-
-    const formattedAddress = useMemo(() => {
-        if (!userInfo?.address) return "";
-        const upperAddress = userInfo?.address.toUpperCase();
-        return formatLongText(upperAddress, 3, 4);
-    }, [userInfo]);
 
     const borderWidth = 1;
     const borderRadius = 4;
@@ -66,9 +60,6 @@ export default function UserInfo() {
     const callbackUrl = useMemo(() => {
         return window.location.origin.includes('localhost') ? window.location.origin : 'https://alpha.nadsa.space/api/twitter_auth';
     }, []);
-
-    console.log('currentRank', currentRank);
-
 
     return (
         <div
@@ -128,7 +119,9 @@ export default function UserInfo() {
                     </div>
 
                     <div className="flex items-center gap-2 mb-4">
-                        <span className="text-white text-[20px]">{formattedAddress}</span>
+                        <span className="text-white text-[20px]">
+                            {formatLongText(userRanking?.user_name || userRanking?.address, !!userRanking?.user_name ? 10 : 5, 4)}
+                        </span>
                         <Copyed value={userInfo?.address || ""} />
                     </div>
 
