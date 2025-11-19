408f6a19b1ea3b934d2d876df1f35b630c4cf2c6
ben <benjm240@gmail.com>
2025-11-19 14:55:51 +0800

    add monday trade v3

---
+++ src/sdk/smart-router/models/mondaytrade-v3.model.ts
@@ -0,0 +1,166 @@
+import BigNumber from "bignumber.js";
+import { utils, providers, Contract } from "ethers";
+import { V3 } from "../libs/v3.lib";
+import chains from "../config/chains";
+import routerV3Abi from "../config/abi/router-v3-4";
+
+export class MondayTradeV3 {
+  private v3: V3;
+
+  private FACTORY: { [key: number]: string } = {
+    10143: "0x749117280Ae615BEacD54Eea4E0d580d5c895538",
+    143: "0xC1e98D0A2a58fB8aBd10ccc30a58efff4080Aa21"
+  };
+  private QUOTER: { [key: number]: string } = {
+    10143: "0xb986260e95FF6C7C57c31B777613F25522D026FC",
+    143: "0xB97eCD41Aef0F842E773C8F9905919cDE49880C9"
+  };  
+  private ROUTER: { [key: number]: string } = {
+    10143: "0xE82aeD04aEb7EEF76CCB985Db73BEe73630fe523",
+    143: "0xFE951b693A2FE54BE5148614B109E316B567632F"
+  };
+  private FEES: { [key: number]: number[] } = {
+    10143: [100, 500, 3000, 10000]
+  };
+  private MID_TOKENS: { [key: number]: any } = {
+    10143: []
+  };
+
+  constructor(chainId: number) {
+    this.v3 = new V3({
+      fees: this.FEES[chainId],
+      chainId,
+      factoryAddress: this.FACTORY[chainId],
+      quoterAddress: this.QUOTER[chainId],
+      midTokens: this.MID_TOKENS[chainId]
+    });
+  }
+  public async quoter({
+    inputCurrency,
+    outputCurrency,
+    inputAmount,
+    slippage,
+    account
+  }: any) {
+    const _amount = BigNumber(inputAmount)
+      .multipliedBy(10 ** inputCurrency.decimals)
+      .toFixed(0);
+    const [bestTradeV3] = await Promise.all([
+      this.v3.bestTrade({
+        inputCurrency,
+        outputCurrency,
+        inputAmount: _amount
+      }),
+    ]);
+
+    console.log('bestTradeV3:', bestTradeV3);
+
+    let bestTrade = bestTradeV3;
+    let routerAddress = this.ROUTER[inputCurrency.chainId];
+    let type = "v3";
+
+   
+
+    if (!bestTrade) {
+      return {
+        outputCurrencyAmount: "",
+        noPair: true
+      };
+    }
+
+    return this.handleV3({
+      bestTrade,
+      outputCurrency,
+      inputCurrency,
+      _amount,
+      slippage,
+      account,
+      routerAddress
+    });
+  }
+
+  private async handleV3({
+    bestTrade,
+    inputCurrency,
+    outputCurrency,
+    _amount,
+    account,
+    slippage
+  }: any) {
+    const outputCurrencyAmount = new BigNumber(bestTrade.amountOut)
+      .div(10 ** outputCurrency.decimals)
+      .toFixed(outputCurrency.decimals)
+      .replace(/\.?0+$/, "");
+
+    const returnData = {
+      outputCurrencyAmount,
+      noPair: false,
+      routerAddress: this.ROUTER[inputCurrency.chainId],
+      routes: bestTrade.routes
+    };
+
+    const RouterIface = new utils.Interface(routerV3Abi);
+    const options = {
+      value: inputCurrency.isNative ? _amount : "0"
+    };
+
+    const _amountOut = BigNumber(bestTrade.amountOut)
+      .multipliedBy(1 - slippage)
+      .toFixed(0);
+
+    const multicallParams = [];
+
+    const inputs = {
+      path: bestTrade.path,
+      recipient: outputCurrency.isNative
+        ? this.ROUTER[inputCurrency.chainId]
+        : account,
+      amountIn: _amount,
+      amountOutMinimum: _amountOut
+    };
+
+    multicallParams.push(
+      RouterIface.encodeFunctionData("exactInput", [inputs])
+    );
+
+    if (outputCurrency.isNative) {
+      multicallParams.push(
+        RouterIface.encodeFunctionData("unwrapWETH9", ["0", account])
+      );
+    }
+
+    const provider = new providers.JsonRpcProvider(
+      chains[inputCurrency.chainId].rpcUrls[0]
+    );
+    const multicallContract = new Contract(
+      this.ROUTER[inputCurrency.chainId],
+      routerV3Abi,
+      provider.getSigner(account)
+    );
+
+    let estimateGas;
+
+    try {
+      estimateGas = await multicallContract.estimateGas.multicall(
+        multicallParams,
+        options
+      );
+    } catch (err) {
+      // console.log('estimateGas err', err);
+    }
+
+    console.log("estimateGas v3", estimateGas?.toString());
+
+    const txn = await multicallContract.populateTransaction.multicall(
+      multicallParams,
+      {
+        ...options,
+        gasLimit: estimateGas
+          ? BigNumber(estimateGas.toString()).multipliedBy(1.2).toFixed(0)
+          : 5000000
+      }
+    );
+
+    return { ...returnData, txn };
+  }
+}
--- src/sections/swap/Content/index.tsx
+++ src/sections/swap/Content/index.tsx
@@ -51,7 +51,8 @@ export default function Swap({
   const { loading, trade, tradeList, onQuoter, onSwap, setTrade } = useTrade({
     chainId: DEFAULT_CHAIN_ID,
     // template: dapp.name,
-    template: isSuperSwap ? ['Uniswap', 'Pancake', 'OneClick', 'iZumi', 'LFJ', 'Kuru', 'MondayTrade'] : dapp.name,
+    template: isSuperSwap ? ['MondayTradeV3', 'Uniswap'] : dapp.name,
+    // template: isSuperSwap ? ['Uniswap', 'Pancake', 'OneClick', 'iZumi', 'LFJ', 'Kuru', 'MondayTrade', 'MondayTradeV3'] : dapp.name,
     from,
     onSuccess: () => {
       setUpdater(Date.now());
