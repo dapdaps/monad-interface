8dede483a5dc23f2265a9a9aaed3ff58e7352534
ben <benjm240@gmail.com>
2025-11-19 11:24:39 +0800

    change swap time

--- src/sections/swap/Content/index.tsx
+++ src/sections/swap/Content/index.tsx
@@ -122,7 +122,7 @@ export default function Swap({
   useEffect(() => {
     const interval = setInterval(() => {
       setRefreshQuoter(Date.now());
-    }, 1000 * 20);
+    }, 1000 * 30);
 
     return () => {
       clearInterval(interval);
