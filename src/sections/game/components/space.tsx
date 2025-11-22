3c6f0583ee62a206c8cdd78fb2123f328b0104d8
ben <benjm240@gmail.com>
2025-11-22 15:37:06 +0800

    remove chart and guess game

--- src/sections/game/components/space.tsx
+++ src/sections/game/components/space.tsx
@@ -7,8 +7,8 @@ export default function Space() {
     
     return (
         <div onClick={() => {
-            router.push("/arcade/space-invaders");
-        }} className="w-[438px] h-[739px] absolute bottom-0 right-[calc(50%-500px)] cursor-pointer bg-[url('/images/mainnet/game/space.png')] bg-no-repeat bg-cover bg-center group">
+            // router.push("/arcade/space-invaders");
+        }} className="opacity-30 w-[438px] h-[739px] absolute bottom-0 right-[calc(50%-500px)] bg-[url('/images/mainnet/game/space.png')] bg-no-repeat bg-cover bg-center group">
             <img 
                 src="/images/mainnet/game/space_hover.png" 
                 className="absolute top-[-10px] left-[8px] w-[320px] pointer-events-none opacity-0 group-hover:opacity-100 transition-opacity duration-200"
