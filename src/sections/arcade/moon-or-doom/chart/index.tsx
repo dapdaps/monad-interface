a23196b63c5299c8366e9381945295f0b3becfaf
ben <benjm240@gmail.com>
2025-11-29 23:16:09 +0800

    change 0.2 -> 0.5

--- src/sections/arcade/moon-or-doom/chart/index.tsx
+++ src/sections/arcade/moon-or-doom/chart/index.tsx
@@ -908,7 +908,7 @@ export default function Chart({ bet, list = [], betList = [], handleBet, betLoad
             if (!isNaN(rectY1)) {
                 const calculatedPrice = yScale.invert(rectY1);
                 const alignedPrice = Math.floor(calculatedPrice / PRICE_STEP) * PRICE_STEP;
-                gridPrice = alignedPrice;
+                gridPrice = alignedPrice - PRICE_STEP;
                 (this as any).__gridPrice__ = Math.floor(gridPrice * 10) / 10;
             }
 
