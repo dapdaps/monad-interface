7e3f880376b900fdd38507263e585ca3d48e2b2a
ben <benjm240@gmail.com>
2025-12-02 17:20:21 +0800

    add wallet info on home

--- src/layouts/mainnet/laptop/index.tsx
+++ src/layouts/mainnet/laptop/index.tsx
@@ -13,7 +13,6 @@ const EXCLUDE_FOOTER_PATHS = [
 const EXCLUDE_WALLET_INFO_PATHS = [
   /^\/wallet/,
   /^\/rank/,
-  /^\/$/,
   /^\/discover$/,
   /^\/arcade\/guess-who$/,
   /^\/arcade\/space-invaders$/,
